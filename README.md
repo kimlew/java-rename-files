8948ccc7a61c7a0f00d6dce1517c9a3cfc873f08
Kim Lew <gmksl7600@gmail.com>
2019-07-15 09:10:40 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Java command-line script - RenameFiles.java
+
+Script for use on local machine that renames files based on 3 user inputs:
+
+- the string you want to replace
+- the replacement string
+- the absolute path or relative path of starting directory
+
+e.g., java com.javarenamefiles.RenameFiles
---
+++ src/com/javarenamefiles/RenameFiles.java
@@ -0,0 +1,36 @@
+package com.javarenamefiles;
+
+import java.util.Scanner;
+
+public class RenameFiles {
+
+    public static void main(String[] args) {
+        getInput();
+        // TODO: renameAllFiles();
+    }
+
+    private static void getInput() {
+        Scanner input = new Scanner(System.in);
+
+        // Use read(), next(), or readLine() to read input.
+        System.out.println("Enter the string you want to replace: ");
+        String string_to_replace = input.next();
+
+        System.out.println("Enter the replacement string: ");
+        String replacement_string = input.next();
+
+        System.out.println("Enter the starting path of the files: ");
+        String starting_path = input.next();
+
+        System.out.println();
+        System.out.println("String to Replace:\t" + string_to_replace);
+        System.out.println("Replacement String:\t" + replacement_string);
+        System.out.println("Starting Path of Files:\t " + starting_path);
+
+    }
+
+    //private static void renameAllFiles() {
+    //
+    //}
+
+} // End of class.
